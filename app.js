5b78f9c15555d7343504dbd78d23232d11628a77
Mihail Heimonen <mihail.heimonen@gmail.com>
2021-04-30 11:14:34 +0300

    update app.js

--- app.js
+++ app.js
@@ -16,6 +16,10 @@ app.use("/e-payment-notify", require("./routes/epaymentnotify.routes"));
 
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
 const PORT = process.env.PORT || 5000;
