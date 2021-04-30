4ef106ee03eb12ef283dbcc37cac5397554dd909
Mihail Heimonen <mihail.heimonen@gmail.com>
2021-04-30 10:46:29 +0300

    update server_url

--- client/src/components/merchantlist.js
+++ client/src/components/merchantlist.js
@@ -2,10 +2,16 @@ import React, { useState, useEffect } from "react";
 import { useHttp } from "../hooks/http.hook";
 
 export const MerchantList = ({ giftId }) => {
+  const SERVER_URL =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:5000"
+      : "https://mivida-app.herokuapp.com";
+
   const { request } = useHttp();
   const [merchant, setMerchant] = useState([]);
 
   const getMerchant = async () => {
+    console.log("server url", SERVER_URL, process.env.NODE_ENV);
     try {
       const fetch = await request(
         "/api/giftcard/get-merchant-payment-methods",
@@ -25,18 +31,13 @@ export const MerchantList = ({ giftId }) => {
   if (giftId) {
     return (
       <div className="container">
-          <h5>Valitse sopiva maksutapa!</h5>
+        <h5>Valitse sopiva maksutapa!</h5>
         <div className="merchantList">
           {merchant.map((item, index) => {
             return (
               <div className="merchantItem" key={index}>
                 <a
-                  href={
-                    "http://localhost:5000/create-charge/" +
-                    item.selected_value +
-                    "/" +
-                    giftId
-                  }
+                  href={`${SERVER_URL}/create-charge/${item.selected_value}/${giftId}`}
                 >
                   <img src={item.img} alt="item" />
                 </a>
