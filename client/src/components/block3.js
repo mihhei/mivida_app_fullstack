8870fe95159034f88a87106d5a1a6498f596cb3e
Mihail Heimonen <mihail.heimonen@gmail.com>
2021-05-31 16:54:32 +0300

    insta update

--- client/src/components/block3.js
+++ client/src/components/block3.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Heading } from "./heading";
 import { InstagramFetching } from "./instagramfetching";
 import { GetMorePicture } from "./getmorepicture";
 
 export const Block3 = () => {
-  const [limit, setLimit] = useState(10);
+  const [limit, setLimit] = useState(0);
   const increaseLimit = () => {
     setLimit((prev) => prev + 5);
   };
+  useEffect(() => {
+  setLimit(10);
+  }, []);
   return (
     <div className="block bl3" id="galleria">
       <Heading value="Galleria" />
