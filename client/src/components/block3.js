3bd3b6095a2789ee35e5f03ef867458fa6d8fb4a
Mihail Heimonen <mihail.heimonen@gmail.com>
2021-06-01 10:21:28 +0300

    background customization

--- client/src/components/block3.js
+++ client/src/components/block3.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Heading } from "./heading";
 import { InstagramFetching } from "./instagramfetching";
 import { GetMorePicture } from "./getmorepicture";
